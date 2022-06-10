ae8b70b0eafd520f58c77504fafb8a834c4fdb5f
Kloong1 <dkdlsmsm98@gmail.com>
2022-06-10 22:49:07 +0900

    first commit

---
+++ src/main/java/com/kloong/retreat/RetreatApplication.java
@@ -0,0 +1,13 @@
+package com.kloong.retreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetreatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetreatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kloong/retreat/RetreatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kloong.retreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetreatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
