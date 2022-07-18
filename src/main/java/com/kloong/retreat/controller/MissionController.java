9a958aa6d2c378ae999e38f537fdcc0d94f90b47
Kloong1 <dkdlsmsm98@gmail.com>
2022-07-18 22:38:57 +0900

    logger send log to slack

--- src/main/java/com/kloong/retreat/controller/MissionController.java
+++ src/main/java/com/kloong/retreat/controller/MissionController.java
@@ -4,6 +4,10 @@ import com.kloong.retreat.log.MissionClearLogger;
 import com.kloong.retreat.password.MissionPasswordChecker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/missions")
@@ -24,16 +28,33 @@ public class MissionController {
         return MISSION_DIR_PREFIX + stone + "/1";
     }
 
-    @PostMapping("/{stone}/{number}")
-    public String mission(@PathVariable String stone, @PathVariable int number, @RequestParam String password) {
-        String currentMission = stone + (number - 1);
+    @PostMapping("/{stone}/{missionNumber}")
+    public String mission(@PathVariable String stone, @PathVariable int missionNumber, @RequestParam String password) {
+        String currentMission = stone + (missionNumber - 1);
 
         if (missionPasswordChecker.checkPassword(currentMission, password)) {
-            missionClearLogger.log(currentMission, password, true);
-            return MISSION_DIR_PREFIX + stone + "/" + number;
+            missionClearLogger.log(stone, missionNumber - 1, password, true);
+            return MISSION_DIR_PREFIX + stone + "/" + missionNumber;
         }
 
-        missionClearLogger.log(currentMission, password, false);
-        return MISSION_DIR_PREFIX + "/wrongPassword";
+        missionClearLogger.log(stone, missionNumber - 1, password, false);
+        return MISSION_DIR_PREFIX + "wrongPassword";
+    }
+
+    @PostMapping("/{stone}/{number}/img")
+    public String uploadImage(@RequestParam MultipartFile image, @PathVariable String stone, @PathVariable String number) {
+        if (!image.isEmpty()) {
+            try {
+                File upload = new File("/Users/kloong/Downloads/image.png");
+                System.out.println(image.getOriginalFilename());
+                System.out.println(image.getSize());
+                System.out.println(upload.getAbsolutePath());
+                image.transferTo(upload);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return "/html/missions/wrongPassword";
     }
 }
--- src/main/java/com/kloong/retreat/log/MissionClearLogger.java
+++ src/main/java/com/kloong/retreat/log/MissionClearLogger.java
@@ -1,9 +1,91 @@
 package com.kloong.retreat.log;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class MissionClearLogger {
-    public void log(String mission, String password, boolean clear) {
+    private final static String WEBHOOKS_FILE_PATH = "data/webhooks.txt";
+
+    private final Map<String, String> webhookMap;
+    private final Map<String, String> stoneMissionMap;
+
+    public MissionClearLogger() {
+        webhookMap = new HashMap<>();
+        stoneMissionMap = new HashMap<>();
+        initWebhookMap();
+        initStoneMissionMap();
+    }
+
+
+    public void log(String stone, int missionNumber, String password, boolean clear) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        String messageBody = getMessageBody(stone, missionNumber, password, clear);
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(messageBody, httpHeaders);
+
+        restTemplate.postForEntity(webhookMap.get(stone), httpEntity, String.class);
+    }
+
+    private String getMessageBody(String stone, int missionNumber, String password, boolean clear) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{\"text\":\"");
+
+        stringBuilder.append(stoneMissionMap.get(stone)).append("-");
+        stringBuilder.append("미션").append(missionNumber).append("-");
+
+        if (clear) {
+            stringBuilder.append("클리어");
+        }
+        else {
+            stringBuilder.append("실패-");
+            stringBuilder.append("입력한 암호:[").append(password).append("]");
+        }
+
+        stringBuilder.append("\"}");
+
+        return stringBuilder.toString();
+    }
+
+    private void initWebhookMap() {
+        File webhooksFile = new File(WEBHOOKS_FILE_PATH);
+
+        if (!webhooksFile.exists()) {
+            throw new RuntimeException("Webhook 파일이 존재하지 않습니다. - " + webhooksFile.getAbsolutePath());
+        }
+
+        try (FileReader fr = new FileReader(webhooksFile);
+             BufferedReader br = new BufferedReader(fr)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] stoneAndWebhook = line.split(";");
+                webhookMap.put(stoneAndWebhook[0], stoneAndWebhook[1]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("잘못된 Webhook 파일입니다. - " + webhooksFile.getAbsolutePath());
+        }
+    }
+
+    private void initStoneMissionMap() {
+        stoneMissionMap.put("disciple", "[디사이플 스톤 | 광화문]");
+        stoneMissionMap.put("vision", "[비전 스톤 | 배재학당]");
+        stoneMissionMap.put("truth", "[트루 스톤 | 연세대]");
+        stoneMissionMap.put("agape", "[아가페 스톤 | 양화진]");
     }
 }
